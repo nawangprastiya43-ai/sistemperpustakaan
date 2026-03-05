d2e758bb40399f213dcfce16370933a1de1adacd
nawangprastiya43-ai <nawangprastiya43@gmail.com>
2026-03-05 10:31:04 +0700

    prosesdata

--- proses_data.php
+++ proses_data.php
@@ -1,56 +1,43 @@
 <?php
+require 'koneksi.php';
 
 if (isset($_POST['submit'])) {
 
-    $judul   = $_POST['judul'];
-    $penulis = $_POST['penulis'];
-    $tahun   = $_POST['tahun'];
-    $status  = $_POST['status'];
+    $judul   = trim($_POST['judul']);
+    $penulis = trim($_POST['penulis']);
+    $tahun   = (int) $_POST['tahun'];
+    $status  = (int) $_POST['status'];
 
-    if (empty($judul) || empty($penulis) || empty($tahun) || empty($status)) {
+    // Validasi
+    if (empty($judul) || empty($penulis) || empty($tahun) || !isset($_POST['status'])) {
 
         echo "<h3 style='color:red;'>Error: Semua kolom wajib diisi!</h3>";
         echo "<a href='tambah_data.php'>Kembali ke Form</a>";
-
-    } else {
-
-        echo "<h2>Data Berhasil Diinput:</h2>";
-
-        echo "<table border='1' cellpadding='8' cellspacing='0'>";
-        echo "<tr>
-                <th>Field</th>
-                <th>Data</th>
-              </tr>";
-
-        echo "<tr>
-                <td>Judul Buku</td>
-                <td>$judul</td>
-              </tr>";
-
-        echo "<tr>
-                <td>Penulis</td>
-                <td>$penulis</td>
-              </tr>";
-
-        echo "<tr>
-                <td>Tahun Terbit</td>
-                <td>$tahun</td>
-              </tr>";
-
-        echo "<tr>
-                <td>Status</td>
-                <td>$status</td>
-              </tr>";
-
-        echo "</table>";
-
-        echo "<br><br>";
-        echo "<a href='tambah_data.php'>Input Data Lagi</a>";
+        exit;
     }
 
-} else {
+    if ($tahun < 2000) {
+        echo "<h3 style='color:red;'>Error: Tahun minimal 2000!</h3>";
+        echo "<a href='tambah_data.php'>Kembali ke Form</a>";
+        exit;
+    }
 
+    // Prepared Statement
+    $query = "INSERT INTO buku (judul_buku, penulis, tahun_terbit, status_pinjam) VALUES (?, ?, ?, ?)";
+    $stmt  = mysqli_prepare($koneksi, $query);
+
+    mysqli_stmt_bind_param($stmt, "ssii", $judul, $penulis, $tahun, $status);
+
+    if (mysqli_stmt_execute($stmt)) {
+        header("Location: index.php");
+        exit;
+    } else {
+        echo "Gagal menyimpan data: " . mysqli_error($koneksi);
+    }
+
+    mysqli_stmt_close($stmt);
+
+} else {
     echo "<h3>Akses tidak valid!</h3>";
 }
-
 ?>
